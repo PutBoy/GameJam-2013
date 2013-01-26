a266698674ae6c30529fe1dfb4210108c2c5efb6
Sean <conk3r_xd@hotmail.com>
2013-01-26 01:28:38 +0100

    fixar filer entity

--- silence/silence/Entity.h
+++ silence/silence/Entity.h
@@ -8,11 +8,11 @@ public:
 	Entity(sf::Vector2f startPos);
 	Entity* GetNextDrop();
 	void Drop(Entity*);
-	virtual ~Entity(void);
+	virtual ~Entity(void){};
 	float getXpos()const;
 	float getYpos()const;
-	virtual void update();
-	virtual void render();
+	virtual void update()=0;
+	virtual void render()=0;
 protected:
 	sf::Vector2f mPos;
 
